8a492104c7da4d450f90813729bbf53a64d264af
Florian Schiessl <florian@floriware.de>
2013-03-05 18:00:05 +0100

    cleaning up, introducing function to wait for server answer, colored own messages

--- chatsimple-client.pl
+++ chatsimple-client.pl
@@ -1,40 +1,64 @@
 #!/usr/bin/perl -w
-# 
+#
 # ChatSimple Client by Florian Schiessl. <florian at floriware.de>
-# 
+#
 # Dev-Start: 23.2.2013
-# 
+#
+# Changelog:
+# 4.3.2013 Florian Schiessl:
+# - removed not working colors for Windows users.
+# 5.3.2013 Florian Schiessl:
+# - cleaning up (use strict added)
+# - added &requestok (function to wait for servers 'ok' answer)
+# - added reverse line feed for own messages
+# - added colors for yourself (cyan) and others (magenta)
 
+use strict;
 use IO::Socket;
 use threads;
 use threads::shared;
+use Thread::Queue;
 use Term::ANSIColor;
+use Term::ReadKey;
 use Time::HiRes qw( usleep );
 
+##################
+# Configuration
+my $sep = '::'; # Separator for commands
+my $default_port = 5060;
+my $default_ip = "127.0.0.1";
+
+##########
+# Start
+
+# If given Argument 0 then ip = Argument 0 else "127.0.0.1".
+# The ? : construct is called Ternary Operator
+my $ip = $ARGV[0] ? $ARGV[0] : $default_ip;
+my $port = $ARGV[1] ? $ARGV[1] : $default_port;
+
+my $username :shared = getpwuid($<); # The used username as shared (between threads) variable. Default = system user.
+my $password :shared; # The server password
+my $ok :shared = 0; # must be >0 for requesting an ok message (see &requestok)
+
+my $okqueue = Thread::Queue->new();
+
 # Catching Signals
-$SIG{INT} = \&tsktsk;
-$SIG{TERM} = \&tsktsk;
-$SIG{QUIT} = \&tsktsk;
-$SIG{ABRT} = \&tsktsk;
-$SIG{HUP} = \&tsktsk;
+$SIG{INT} = \&exittsk;
+$SIG{TERM} = \&exittsk;
+$SIG{QUIT} = \&exittsk;
+$SIG{ABRT} = \&exittsk;
+$SIG{HUP} = \&exittsk;
 
 # Signal action
-sub tsktsk
+sub exittsk
 {
+	ReadMode(0);
 	print "\nBye.\n";
 	exit 0;
 }
 
-# If given Argument 0 then ip = Argument 0 else "127.0.0.1". Google Ternary Operator if curious.
-$ip = $ARGV[0] ? $ARGV[0] : "127.0.0.1";
-$port = $ARGV[1] ? $ARGV[1] : "5060";
-
-my $username :shared; # The used username as shared (between threads) variable.
-my $password :shared; # The server password
-my $ok :shared; # Is ok, if last message from server was "ok"
-
 # Welcome message
-&textcolor('bold blue',"ChatSimple Client v.1.0
+&textcolor('bold blue',"ChatSimple Client v.1.1.0
 IP: $ip
 Port: $port
 Exit: Ctrl+C");
@@ -51,41 +75,43 @@ die "Unable to open connection: $!\n" unless defined $socket;
 # the connection was established.
 &textcolor('green',"Connection established.\n");
 
-# ask for username and password unless the server answer is "ok"
+my $result;
+# ask for username and password until the server answer is "ok"
 do
 {
-	&textcolor('yellow',"Username: ");
-	$username = <STDIN>;
+	&textcolor('yellow',"Username (".$username."): ");
+	my $new_username = <STDIN>;
+	$username = $new_username ne "\n" ? $new_username : $username;
 	&textcolor('yellow',"Server Password: ");
-	$password = <STDIN>;
+	$password = &getpassword;
 	chomp($username);
 	chomp($password);
 
 	# try to log in
-	print $socket "login::".$username."::".$password."\n";
+	print $socket "login".$sep.$username.$sep.$password."\n";
 	
 	# getting server's answer
 	$result = <$socket>;
 	chomp($result);
-	@data = split("::",$result);
+	my @data = split($sep,$result);
 	if($data[0] eq "err")
 	{
 		&textcolor('red',$data[2]."\n"); # Show error message to user
 	}
 }
-while ($result ne "ok");
+until($result eq "ok");
+undef $result; # clear up for eventually later use
 
 # Login successfull
 &textcolor('green',"Login successfull.\n");
 
 # Starting new Thread, which handles incomming messages from the server
-$thread = threads->new(\&thread,$socket);
+my $thread = threads->new(\&thread,$socket);
 $thread->detach;
 
 # Wait for user input
-while(<STDIN>)
+while(my $line = <STDIN>)
 {
-	$line = $_;
 	chomp($line);
 	
 	# Check if line starts with / (command mode)
@@ -95,10 +121,10 @@ while(<STDIN>)
 		if($line =~ s/rename //g)
 		{
 			$ok = "needed"; # reset $ok, so that it is not ok anymore
-			print $socket "rename::".$username."::".$line."\n";
-			usleep(50000); # wait for the server answer
+			print $socket "rename".$sep.$username.$sep.$line."\n";
+			my $success = &requestok(1); # check, if first (1) answer from server after this was 'ok'
 			# check if the answer was ok
-			if($ok eq "ok")
+			if($success eq "ok")
 			{
 				$username = $line;
 			}
@@ -113,14 +139,21 @@ while(<STDIN>)
 		# logout with logout message
 		elsif($line =~ s/logout //g)
 		{
-			print $socket "logout::".$username."::".$line."\n";
+			print $socket "logout".$sep.$username.$sep.$line."\n";
 			
 		}
 		
 		# generic logout (without message)
-		elsif($line =~ s/logout//g)
+		elsif($line =~ m/logout/)
 		{
-			print $socket "logout::".$username."\n";
+			print $socket "logout".$sep.$username."\n";
+		}
+		
+		# exit
+		elsif($line =~ m/exit/ || $line =~ m/quit/)
+		{
+			print $socket "logout".$sep.$username."\n";
+			&exittsk;
 		}
 
 		# and all unknown commands.
@@ -134,52 +167,82 @@ while(<STDIN>)
 	# else interpret input as normal message
 	else
 	{
-		print $socket "say::".$username."::".$line."\n";
+		print $socket "say".$sep.$username.$sep.$line."\n";
 	}
-}
 
+}
+&textcolor('red',"This should not happen!");
+print "\n";
+exit 1;
+
+# The thread. It takes up all incoming messages from the server
+# and print them to the user's terminal.
 sub thread
 {
 	$socket = shift;
 	while(<$socket>)
 	{
-		$line = $_;
-		chomp($line);
-		@data = split("::",$line);
-		if(!defined($data[0])){$data[0] = "";}
-		if(!defined($data[1])){$data[1] = "";}
-		if(!defined($data[2])){$data[2] = "";}
-		if($data[0] eq "msg" && $data[1] ne $username)
+		my $line = $_;
+		chomp($line); # Remove newline
+		my @data = split($sep,$line); # Split up answer
+		$data[0] = "" if !$data[0];
+		$data[1] = "" if !$data[1];
+		$data[2] = "" if !$data[2];
+		
+		# Incomming chatmessage
+		if($data[0] eq "msg")
 		{
-			print &timestamp." ";
-			&textcolor('bold',$data[1].": ");
+			print "\e[A" if $data[1] eq $username; # reverse line feed
+			&newinput;
+			&textcolor($data[1] eq $username ? 'cyan' : 'magenta',$data[1].": ");
 			print $data[2]."\n";
 		}
+		
+		# Incoming notification
 		elsif($data[0] eq "notify")
 		{
-			print &timestamp." ";
+			&newinput;
 			&textcolor('bold yellow',$data[1].": ".$data[2]);
 			print "\n";
 		}
+		
+		# Incoming error message
 		elsif($data[0] eq "err")
 		{
-			print &timestamp." ";
+			&newinput;
 			&textcolor('bold red',$data[1].": ".$data[2]);
 			print "\n";
 		}
-		elsif($data[0] eq "ok")
-		{
-			$ok = "ok";
-		}
+		
+		# Incoming client list
 		elsif($data[0] eq "clients")
 		{
-			print &timestamp." ";
+			&newinput;
 			&textcolor('yellow',"Connected Clients:");
 			print join(" ",split(",",$data[1]))."\n";
 		}
+		
+		# Incoming ok
+		elsif($data[0] eq "ok")
+		{
+			if($ok >= 1)
+			{
+				# an ok was requested
+				$ok = 0;
+				$okqueue->enqueue('ok'); # Send an answer
+			}
+		}
+		
+		# decrease $ok if message was not 'ok'
+		if($ok > 0)
+		{
+			$okqueue->enqueue('err') if(--$ok == 0); # no ok message was within
+		}
 	}
+	
+	# The connection to the Server was lost.
 	&textcolor('red',"\nConnection lost!\n");
-	exit 0;
+	&exittsk;
 }
 
 # Print text with fancy colors
@@ -201,6 +264,13 @@ sub textcolor
 	}
 }
 
+# Print default input heading
+sub newinput
+{
+	print "\r\e[K"; # Clear last Line
+	print &timestamp." ";
+}
+
 # A simple Timestamp
 sub timestamp
 {
@@ -210,31 +280,72 @@ sub timestamp
 # Returns a part of the current date
 sub dt
 {
-        @localtime=localtime(time);
-        if ($_[0] eq 'year')
-        {return $localtime[5]+1900}
-        elsif ($_[0] eq 'month')
-        {return &attachleading(2,$localtime[4]+1)}
-        elsif ($_[0] eq 'day')
-        {return &attachleading(2,$localtime[3])}
-        elsif ($_[0] eq 'hour')
-        {return &attachleading(2,$localtime[2])}
-        elsif ($_[0] eq 'minute')
-        {return &attachleading(2,$localtime[1])}
-        elsif ($_[0] eq 'second')
-        {return &attachleading(2,$localtime[0])}
-        else {return "dterror"}
+	my @localtime=localtime(time);
+	if ($_[0] eq 'year')
+	{return $localtime[5]+1900}
+	elsif ($_[0] eq 'month')
+	{return &attachleading(2,$localtime[4]+1)}
+	elsif ($_[0] eq 'day')
+	{return &attachleading(2,$localtime[3])}
+	elsif ($_[0] eq 'hour')
+	{return &attachleading(2,$localtime[2])}
+	elsif ($_[0] eq 'minute')
+	{return &attachleading(2,$localtime[1])}
+	elsif ($_[0] eq 'second')
+	{return &attachleading(2,$localtime[0])}
+	else {return "dterror"}
 }
 
 # attaches a leading 0 to input
 sub attachleading
 {
-        (my $length, my $string) = @_;
-        for (my $count = $length - length($string); $count>0; --$count)
-        {
-                $string = "0$string";
-        }
-        return $string;
+	(my $length, my $input) = @_;
+	return sprintf("%0".$length."d",$input);
+}
+
+sub requestok
+{
+	$ok = shift; # the count of next messages from Server the ok could be within
+	while(my $success = $okqueue->dequeue())
+	{
+		return $success;
+	}
+}
+
+# Get password without showing it to user
+# Adapted from http://stackoverflow.com/questions/701078/how-can-i-enter-a-password-using-perl-and-replace-the-characters-with
+sub getpassword
+{
+	my $password = "";
+	my $key;
+	# Start reading the keys
+	ReadMode(4); #Disable the control keys
+	while(ord($key = ReadKey(0)) != 10)
+	# This will continue until the Enter key is pressed (decimal value of 10)
+	{
+		# For all value of ord($key) see http://www.asciitable.com/
+		if(ord($key) == 127 || ord($key) == 8)
+		{
+			# DEL/Backspace was pressed
+			#1. Remove the last char from the password
+			chop($password);
+			#2 move the cursor back by one, print a blank character, move the cursor back by one
+			#print "\b \b";
+		}
+		elsif(ord($key) < 32)
+		{
+			# Do nothing with these control characters
+		}
+		else
+		{
+			$password = $password.$key;
+		#	print $key;
+		#	print "*(".ord($key).")";
+		}
+	}
+	ReadMode(0); #Reset the terminal once we are done
+	print "\n";
+	return $password;
 }
 
 __END__
